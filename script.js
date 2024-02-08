afdaf26642fb33c4cff281ea858c3c627d02c273
Louis Lee <80302893+louislab@users.noreply.github.com>
2024-02-08 12:14:33 +0800

    Updated layout

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener('scroll', (e) => {
     // blur on scroll
-    const header = document.querySelector('.header');
+    const header = document.querySelector('.fixed-container');
     const scroll = window.scrollY;
     const blur = scroll / 100;
     header.style.filter = `blur(${blur}px)`;
     
-    const content = document.querySelector('#content');
+    const content = document.querySelector('.content');
     content.classList.remove('poping');
 })
